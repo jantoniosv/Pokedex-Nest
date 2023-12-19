2dfa71a1f17eba74f4f9dfa9127bb0439068a2c3
José Antonio Serrano Ventura <jose.antonio.serrano@copyleft.com.mx>
2023-12-19 15:59:05 -0600

    Use enviroment config on pokemon

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestj
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,23 +1,36 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
+
+import { isValidObjectId, Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model, isValidObjectId } from 'mongoose';
-import { Pokemon } from './entities/pokemon.entity';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
+
+  private defaultLimit: number;
 
   constructor(
 
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
 
-  ) {}
+    private readonly configService: ConfigService,
+
+  ) {
+
+    this.defaultLimit = configService.get<number>('defaultLimit') ;
+    // console.log({ defaultLimit: configService.get<number>('defaultLimit') })
+  }
+
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+
     try {
       const pokemon = await this.pokemonModel.create( createPokemonDto );
       return pokemon;
@@ -25,11 +38,15 @@ export class PokemonService {
     } catch (error) {
       this.handleExceptions( error );
     }
+
   }
+
+
 
   findAll( paginationDto: PaginationDto ) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit( limit )
@@ -42,22 +59,27 @@ export class PokemonService {
   }
 
   async findOne(term: string) {
+
     let pokemon: Pokemon;
 
     if ( !isNaN(+term) ) {
       pokemon = await this.pokemonModel.findOne({ no: term });
     }
-    // Validate MongoID
+
+    // MongoID
     if ( !pokemon && isValidObjectId( term ) ) {
       pokemon = await this.pokemonModel.findById( term );
     }
-    // Validate Name
+
+    // Name
     if ( !pokemon ) {
       pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() })
     }
 
+
     if ( !pokemon )
       throw new NotFoundException(`Pokemon with id, name or no "${ term }" not found`);
+
 
     return pokemon;
   }
@@ -78,12 +100,17 @@ export class PokemonService {
   }
 
   async remove( id: string) {
+    // const pokemon = await this.findOne( id );
+    // await pokemon.deleteOne();
+    // return { id };
+    // const result = await this.pokemonModel.findByIdAndDelete( id );
     const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
     if ( deletedCount === 0 )
       throw new BadRequestException(`Pokemon with id "${ id }" not found`);
 
     return;
   }
+
 
   private handleExceptions( error: any ) {
     if ( error.code === 11000 ) {
@@ -92,4 +119,5 @@ export class PokemonService {
     console.log(error);
     throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
   }
+
 }
